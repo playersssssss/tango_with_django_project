5324cdbcd17db2e221a024ca915cab2ca4d93efd
Junquan Chen <2982317C@student.gla.ac.uk>
2025-02-14 04:24:51 +0000

    finish chapter5

--- rango/urls.py
+++ rango/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from rango import views
+from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
 
 app_name = 'rango'
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
